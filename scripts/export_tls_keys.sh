67f9bb73f5f1d255320c5cb73f8d1e3bf9e323be
dchau360 <51533748+dchau360@users.noreply.github.com>
2023-06-13 16:55:46 -0700

    Update export_tls_keys.sh

--- scripts/export_tls_keys.sh
+++ scripts/export_tls_keys.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-###https://blog.kubovy.eu/2020/05/16/retrieve-tls-certificates-from-kubernetes/
 #Get all tls keys from a k8s namespace
 
 NAMESPACE="external-tls"
+mkdir ~/tls-certs
 
 while IFS= read -r SECRET_NAME
 do
     # Export TLS Cert
-    kubectl get secret -n "${NAMESPACE}" "${SECRET_NAME}" -o json | jq -r '.data."tls.crt"' | base64 -d > "${SECRET_NAME}.crt"
+    kubectl get secret -n "${NAMESPACE}" "${SECRET_NAME}" -o json | jq -r '.data."tls.crt"' | base64 -d > ~/tls-certs/"${SECRET_NAME}.crt"
 
     # Export Private Key
-    kubectl get secret -n "${NAMESPACE}" "${SECRET_NAME}" -o json | jq -r '.data."tls.key"' | base64 -d > "${SECRET_NAME}.key"
+    kubectl get secret -n "${NAMESPACE}" "${SECRET_NAME}" -o json | jq -r '.data."tls.key"' | base64 -d > ~/tls-certs/"${SECRET_NAME}.key"
 
 done < <(kubectl get secret -n "${NAMESPACE}" |  awk 'NR>1 {print $1}')
